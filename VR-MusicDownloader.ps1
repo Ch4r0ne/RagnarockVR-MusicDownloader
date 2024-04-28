e7ffc85412c7d3ed337885ef8b5558a784e6e840
Ch4r0ne <54702161+Ch4r0ne@users.noreply.github.com>
2024-04-28 21:53:55 +0200

    Update VR-MusicDownloader.ps1

--- VR-MusicDownloader.ps1
+++ VR-MusicDownloader.ps1
@@ -4,7 +4,22 @@ $startIndex = 3600
 $endIndex = 9
 
 # Zielverzeichnis für die heruntergeladenen Dateien
-$targetDirectory = "C:\Users\leini\Downloads"
+$targetDirectory = "C:\Users\leini\Downloads\temp1"
+
+# Funktion zur Überprüfung, ob eine .dat-Datei im Ordner vorhanden ist
+function Test-DatFile {
+    param (
+        [string]$folderPath
+    )
+    
+    $datFiles = Get-ChildItem -Path $folderPath -Filter *.dat
+    
+    if ($datFiles.Count -gt 0) {
+        return $true
+    } else {
+        return $false
+    }
+}
 
 # Schleife für den Dateidownload
 for ($i = $startIndex; $i -ge $endIndex; $i--) {
@@ -20,9 +35,36 @@ for ($i = $startIndex; $i -ge $endIndex; $i--) {
         
         # Ausgabe, dass der Download erfolgreich war
         Write-Host "Download von Datei $i erfolgreich abgeschlossen. Datei wurde unter $destination gespeichert."
+        
+        # ZIP-Datei extrahieren
+        $extractedFolder = Expand-Archive -Path $destination -DestinationPath $targetDirectory -Force
+        
+        # Überprüfen, ob eine .dat-Datei im extrahierten Ordner vorhanden ist
+        if (Test-DatFile -folderPath $extractedFolder) {
+            Write-Host "Korrekte Datei gefunden im Ordner: $extractedFolder"
+        } else {
+            Write-Host "Keine korrekte Datei gefunden im Ordner: $extractedFolder"
+            
+            # Falls eine .dat-Datei nicht gefunden wurde, kopiere den Inhalt des Ordners auf die oberste Ebene
+            $subfolders = Get-ChildItem -Path $extractedFolder -Directory
+            foreach ($subfolder in $subfolders) {
+                $subfolderFiles = Get-ChildItem -Path $subfolder.FullName -Recurse
+                
+                foreach ($file in $subfolderFiles) {
+                    $newFilePath = Join-Path -Path $targetDirectory -ChildPath $file.Name
+                    Move-Item -Path $file.FullName -Destination $newFilePath -Force
+                }
+            }
+            
+            # Bereinigen des leeren Ordners
+            Remove-Item -Path $extractedFolder -Recurse -Force
+        }
+        
+        # Bereinigen der ZIP-Datei nach dem Extrahieren
+        Remove-Item $destination -Force
     }
     catch {
         # Ausgabe, falls ein Fehler auftritt
-        Write-Host "Fehler beim Download von Datei $i."
+        Write-Host "Fehler beim Verarbeiten von Datei $($i): $($_.Exception.Message)"
     }
 }
